65ca30787a689968960880d67735ef60e3509d0c
Mark Weiss <markweiss@Marks-MacBook-Pro.local>
2022-09-14 09:51:12 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="reeses",
+    version="0.0.1",
+    author="Mark Weiss",
+    author_email="mark.s.weiss.ma@gmail.com",
+    description=open("README.md").read(),
+    url="http://github.com/marksweissma/numenor",
+    packages=find_packages(),
+    install_requires=["scikit-learn", "pandas", "attrs", "fire", "variants"],
+    summary="ml workflow tooling",
+    license="MIT",
+    zip_safe=False,
+)
