514767c1de61b26ca2533b72b922e3830daf906b
Mark Weiss <markweiss@Marks-MacBook-Pro.local>
2022-09-14 11:11:05 -0400

    enforce Column through Factory

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,34 +1,18 @@
-import pytest
 from functools import singledispatch
-from numenor.utils import enforce_first_arg, as_factory, append_key, compose_decorators
+
+import pytest
 from wrapt import decorator
 
-
-def test_compose_decorators():
-
-    def annotate(value):
-
-        @decorator
-        def wrapper(wrapped, instance, args, kwargs):
-            return [value] + wrapped(*args, **kwargs) + [value]
-
-        return wrapper
-
-    @compose_decorators(annotate('first'), annotate('second'))
-    def func(value='func'):
-        return [value]
-
-    assert func() == ['first', 'second', 'func', 'second', 'first']
+from numenor.utils import as_factory, enforce_first_arg
 
 
 def test_enforce_first_arg():
-
     @enforce_first_arg
     def func(a, **kwargs):
         return a
 
-    payload = {'b': 1, 'a': 2}
-    assert func(**payload) == payload['a']
+    payload = {"b": 1, "a": 2}
+    assert func(**payload) == payload["a"]
 
     @singledispatch
     def broken_func(a, **kwargs):
@@ -49,10 +33,10 @@ def test_enforce_first_arg():
     def fixed_func(a, **kwargs):
         return a
 
-    assert fixed_func(**payload) == payload['a']
+    assert fixed_func(**payload) == payload["a"]
 
     @fixed_func.register(int)
     def fixed_func_str(a, **kwargs):
         return -a
 
-    assert fixed_func(**payload) == -payload['a']
+    assert fixed_func(**payload) == -payload["a"]
