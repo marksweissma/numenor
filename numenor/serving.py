324f51d79c113eeafe5da97e622a3224247235e7
marksweissma <markweiss@Marks-MacBook-Pro.local>
2022-10-04 16:22:39 -0400

    add schema infer to estimator and response packaging to prediction

--- numenor/serving.py
+++ numenor/serving.py
@@ -1,81 +1,68 @@
-from attrs import define, field, Factory
-from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.pipeline import Pipeline
 from functools import singledispatch
+from typing import *
+
+import pandas as pd
+import variants
+from attrs import define, field
+from numpy.typing import ArrayLike
+from pydantic import BaseModel
+from sklearn.base import BaseEstimator
+
+from numenor.estimate import Estimator
 
 
+@variants.primary
+def package_prediction(variant="infer", model=None, prediction=None, **kwargs):
+    return getattr(package_prediction, variant)(model, prediction, **kwargs)
+
+
+@package_prediction.variant("infer")
+def package_prediction_infer(model: BaseEstimator, prediction: ArrayLike):
+    if prediction.ndim == 1 or prediction.ndim == 2 and prediction.shape[1] == 1:  # type: ignore
+        result = package_prediction.one_dimensional(model, prediction)
+
+    elif prediction.ndim == 2:  # type: ignore
+        result = package_prediction.two_dimensional(model, prediction)
+    else:
+        result = {"prediction": prediction}
+
+
+@package_prediction.variant("one_dimensional")
+def package_prediction_one_dimensional(model: BaseEstimator, prediction: ArrayLike):  # type: ignore
+    prediction = prediction[0] if prediction.ndim == 1 else prediction[0, 0]  # type: ignore
+    return {"prediction": prediction}
+
+
+@package_prediction.variant("two_dimensional")
 @singledispatch
-def package_predictions(model, prediction):
-    if isinstance(prediction, np.ndarray) and prediction.shape == (1, ):
-        prediction = prediction[0]
-    return {'prediction': prediction}
-
-
-@package_predictions.register(ClassifierMixin)
-def package_predictions(model, prediction):
-    classes = model.classes_
-    if isinstance(
-            prediction,
-            np.ndarray) and prediction.ndim == 2 and prediction.shape[0] == 1:
-        probabilities = {
-            class_: prediction[0][idx]
-            for idx, class_ in enumerate(classes)
+def package_prediction_two_dimensional(model: BaseEstimator, prediction: ArrayLike):
+    if hasattr(model, "classes_"):
+        keys = model.classes_  # type: ignore
+        key_name = "probabilities"
+        probabilities = {class_: prediction[0, idx] for idx, class_ in enumerate(classes)}  # type: ignore
+        prediction = classes[prediction[0, :].argmax()]  # type: ignore
+        payload = {
+            "probabilities": probabilities,
+            "prediction": prediction,
         }
-
-    if isinstance(
-            prediction,
-            np.ndarray) and prediction.ndim == 2 and prediction.shape[0] == 1:
-        prediction = classes[prediction[0, :].argmax()]
-
-    return {
-        'probabilities': probabilities,
-        'argmax_class': classes[prediction[0, :]],
-    }
+    else:
+        payload = {"prediction": prediction}
+    return payload
 
 
-def pandas_feature_converter(features):
-    return pd.DataFrame.from_dict(features, orient='index')
+def pydantic_to_pandas_feature_converter(features: BaseModel):
+    return pd.DataFrame.from_dict(features.dict(), orient="index")
 
 
 @define
-class Serving:
-    transformer: BaseEstimator
-    feature_schema: Dict = field()
-
-    @feature_schema.default
-    def default_feature_schema(self):
-        ...
-
-    response_schema: Dict = field()
-
-    @response_schema.default
-    def default_response_schema(self):
-        ...
-
-    feature_converter: Callable = pandas_feature_converter
-    respone: Callable = package_predictions
+class Serve:
+    estimator: Estimator
+    feature_schema: Dict
+    response_schema: Dict
+    feature_converter: Callable = pydantic_to_pandas_feature_converter
+    response: Callable = package_prediction
 
     def __call__(self, features, *args, **kwargs):
         features = self.feature_converter(features)
-        return self.response(transformer, features, *args, **kwargs)
-
-    @classmethod
-    def infer_from_transformer(
-            cls,
-            transformer,
-            instance_check: Callable = lambda x: isinstance(x, Pipeline),
-            **extras):
-        model = transformer.executor
-        while instance_check(model):
-            model = model[-1]
-        return cls.infer_from_model(model)
-
-    @classmethod
-    def infer_from_model(cls, model, **extras):
-        ...
-        schema = {'model_id': str}
-
-    def from_serving_response(self, **extras):
-        schema = self.schema.copy()
-        schema.update(extras)
-        return evolve(self, schema=schema)
+        prediction = self.estimator.response(features)
+        return self.response(self.estimator, prediction, *args, **kwargs)
