2f50c18e2843fbc11fa8d8c0612ea87100cccd99
marksweissma <mark.weiss.mof@gmail.com>
2022-10-25 20:34:49 -0400

    fix name overloading and end to ends to test suite (#24)

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,10 +1,12 @@
 import pytest
 
-from tutorials import without_config
+from tutorials import test_script, without_config
 
 
 @pytest.mark.example
-@pytest.mark.parametrize("example", [without_config.regression_example])
+@pytest.mark.parametrize(
+    "example", [without_config.regression_example, test_script.test_end_to_end]
+)
 def test_examples_without_config(example):
     variants = example._variants
     [example(variant, plot=False) for variant in variants]
